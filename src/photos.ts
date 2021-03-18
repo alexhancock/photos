012411696e299b5a0295f44a3db4d3e79660cd49
Alex Hancock <alexhancock@squareup.com>
2021-03-17 21:52:25 -0700

    [app] Empty vite/react/ts app and photos in json form

---
+++ src/photos.ts
@@ -0,0 +1,610 @@
+export default [
+  {
+    coords: { '0': 0, '1': 0 },
+    pid: 'vietnam-cambodia-thailand',
+    source: '',
+    title: 'Vietnam Cambodia Thailand',
+    order: 82,
+    vid: 'nxmo2V8llRk'
+  },
+  {
+    coords: { '0': -23.232684, '1': -45.166182 },
+    pid: 'fazenda-catucaba',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/fazenda-catucaba.jpg',
+    title: 'Fazenda Catuçaba',
+    order: 81,
+    vid: ''
+  },
+  {
+    coords: { '0': -23.233576, '1': -45.167546 },
+    pid: 'fazenda-grounds',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/fazenda-grounds.jpg',
+    title: 'Fazenda Grounds',
+    order: 80,
+    vid: ''
+  },
+  {
+    coords: { '0': -23.2335605, '1': -45.1674436 },
+    pid: 'chickens',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/chickens.jpg',
+    title: 'Chickens',
+    order: 79,
+    vid: ''
+  },
+  {
+    coords: { '0': -22.952281, '1': -43.210357 },
+    pid: 'avenida-epitacio-pessoa',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/avenida-epitacio-pessoa.jpg',
+    title: 'Avenida Epitácio Pessoa',
+    order: 78,
+    vid: ''
+  },
+  {
+    coords: { '0': -22.920146, '1': -43.182408 },
+    pid: 'claire-in-santa-teresa',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/claire-in-santa-teresa.jpg',
+    title: 'Claire in Santa Teresa',
+    order: 77,
+    vid: ''
+  },
+  {
+    coords: { '0': -22.912878, '1': -43.183467 },
+    pid: 'rio-paint-crew',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/rio-paint-crew.jpg',
+    title: 'Rio Paint Crew',
+    order: 76,
+    vid: ''
+  },
+  {
+    coords: { '0': 48.855571, '1': 2.337664 },
+    pid: 'street-painting',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/street-painting.jpg',
+    title: 'Street Painting',
+    order: 75,
+    vid: ''
+  },
+  {
+    coords: { '0': 48.885667, '1': 2.3433 },
+    pid: 'paris',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/paris.jpg',
+    title: 'Paris',
+    order: 74,
+    vid: ''
+  },
+  {
+    coords: { '0': 48.858298, '1': 2.311996 },
+    pid: 'les-invalides',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/les-invalides.jpg',
+    title: 'Les Invalides',
+    order: 73,
+    vid: ''
+  },
+  {
+    coords: { '0': 43.525247, '1': 5.448029 },
+    pid: 'aix-en-provence',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/aix-en-provence.jpg',
+    title: 'Aix en Provence',
+    order: 72,
+    vid: ''
+  },
+  {
+    coords: { '0': 43.213158, '1': 5.531542 },
+    pid: 'cassis',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/cassis.jpg',
+    title: 'Cassis',
+    order: 71,
+    vid: ''
+  },
+  {
+    coords: { '0': 43.288085, '1': 5.371148 },
+    pid: 'french-apartments',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/french-apartments.jpg',
+    title: 'French Apartments',
+    order: 70,
+    vid: ''
+  },
+  {
+    coords: { '0': 43.298186, '1': 5.369576 },
+    pid: 'vieux-port',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/vieux-port.jpg',
+    title: 'Vieux Port',
+    order: 69,
+    vid: ''
+  },
+  {
+    coords: { '0': 30.236961, '1': 120.136319 },
+    pid: 'hangzhou-west-lake',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/hangzhou-west-lake.jpg',
+    title: 'Hangzhou - West Lake',
+    order: 68,
+    vid: ''
+  },
+  {
+    coords: { '0': 30.231651, '1': 120.149679 },
+    pid: 'leifeng-pagoda',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/leifeng-pagoda.jpg',
+    title: 'Leifeng Pagoda',
+    order: 67,
+    vid: ''
+  },
+  {
+    coords: { '0': 30.25144, '1': 120.128263 },
+    pid: 'hangzhou-pagodas',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/hangzhou-pagodas.jpg',
+    title: 'Hangzhou Pagodas',
+    order: 66,
+    vid: ''
+  },
+  {
+    coords: { '0': 31.195998, '1': 121.31781 },
+    pid: 'hongqiao-station',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/hongqiao-station.jpg',
+    title: 'Hongqiao Station',
+    order: 65,
+    vid: ''
+  },
+  {
+    coords: { '0': 31.217672, '1': 121.471962 },
+    pid: 'shanghai-butcher',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/shanghai-butcher.jpg',
+    title: 'Shanghai Butcher',
+    order: 64,
+    vid: ''
+  },
+  {
+    coords: { '0': 31.220865, '1': 121.445698 },
+    pid: 'shanghai-traffic',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/shanghai-traffic.jpg',
+    title: 'Shanghai Traffic',
+    order: 63,
+    vid: ''
+  },
+  {
+    coords: { '0': 31.245661, '1': 121.485892 },
+    pid: 'shanghai-at-night',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/shanghai-at-night.jpg',
+    title: 'Shanghai at Night',
+    order: 62,
+    vid: ''
+  },
+  {
+    coords: { '0': 31.225958, '1': 121.481917 },
+    pid: 'french-concession',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/french-concession.jpg',
+    title: 'French Concession',
+    order: 61,
+    vid: ''
+  },
+  {
+    coords: { '0': 31.23621, '1': 121.48349 },
+    pid: 'shanghai-vendors',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/shanghai-vendors.jpg',
+    title: 'Shanghai Vendors',
+    order: 60,
+    vid: ''
+  },
+  {
+    coords: { '0': 37.804132, '1': -122.271267 },
+    pid: 'trayvon-protest',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/trayvon-protest.jpg',
+    title: 'Trayvon Martin',
+    order: 59,
+    vid: ''
+  },
+  {
+    coords: { '0': 20.2383627, '1': 85.833472 },
+    pid: 'bhubaneswar-dogs',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/bhubaneswar-dogs.jpg',
+    title: 'Bhubaneswar Dogs',
+    order: 58,
+    vid: ''
+  },
+  {
+    coords: { '0': 20.3218148, '1': 85.9602594 },
+    pid: 'odisha-vegetable-market',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/odisha-vegetable-market.jpg',
+    title: 'Odisha Vegetable Market',
+    order: 57,
+    vid: ''
+  },
+  {
+    coords: { '0': 22.6061214, '1': 88.3432813 },
+    pid: 'rickshaw-mechanic',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/rickshaw-mechanic.jpg',
+    title: 'Rickshaw Mechanic',
+    order: 56,
+    vid: ''
+  },
+  {
+    coords: [ 23.393825, 88.48098 ],
+    pid: 'summer-swim',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/summer-swim.jpg',
+    title: 'Summer Swim',
+    order: 55
+  },
+  {
+    coords: [ 24.860672, 88.017814 ],
+    pid: 'street-food-vendor',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/street-food-vendor.jpg',
+    title: 'Street Food Vendor',
+    order: 54
+  },
+  {
+    coords: [ 24.809224, 87.953333 ],
+    pid: 'west-bengal-mosque',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/west-bengal-mosque.jpg',
+    title: 'West Bengal Mosque',
+    order: 53
+  },
+  {
+    coords: [ 26.3299676, 89.4466307 ],
+    pid: 'teachers-march',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/teachers-march.jpg',
+    title: "Teacher's March",
+    order: 52
+  },
+  {
+    coords: [ 26.139662, 89.461305 ],
+    pid: 'madan-mohan-temple',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/madan-mohan-temple.jpg',
+    title: 'Madan Mohan Temple',
+    order: 51
+  },
+  {
+    coords: [ 26.32696, 89.43952 ],
+    pid: 'cooch-behar-palace',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/cooch-behar-palace.jpg',
+    title: 'Cooch Behar Palace',
+    order: 50
+  },
+  {
+    coords: [ 26.322441, 89.443458 ],
+    pid: 'rickshaw-driver',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/rickshaw-driver.jpg',
+    title: 'Rickshaw Driver',
+    order: 49
+  },
+  {
+    coords: [ 26.3086794, 89.6649694 ],
+    pid: 'bengali-men',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/bengali-men.jpg',
+    title: 'Bengali Men',
+    order: 48
+  },
+  {
+    coords: [ 26.4747049, 90.7249529 ],
+    pid: 'rice-farmers-assam',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/rice-farmers-assam.jpg',
+    title: 'Rice Farmers - Assam, India',
+    order: 47
+  },
+  {
+    coords: [ 26.275521, 91.1719673 ],
+    pid: 'assam-cow',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/assam-cow.jpg',
+    title: 'Assam Cow',
+    order: 46
+  },
+  {
+    coords: [ 26.1792286, 91.7532732 ],
+    pid: 'indian-butcher',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/indian-butcher.jpg',
+    title: 'Indian Butcher',
+    order: 45
+  },
+  {
+    coords: [ 25.7807028, 91.8751056 ],
+    pid: 'boys-on-bike-meghalaya',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/boys-on-bike-meghalaya.jpg',
+    title: 'Boys on Bike - Meghalaya, India',
+    order: 44
+  },
+  {
+    coords: [ 25.7741938, 91.8748579 ],
+    pid: 'boy-running-meghalaya',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/boy-running-meghalaya.jpg',
+    title: 'Boy Running - Meghalaya, India',
+    order: 43
+  },
+  {
+    coords: [ 25.6034546, 91.8698113 ],
+    pid: 'shillong-outskirts',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/shillong-outskirts.jpg',
+    title: 'Shillong Outskirts',
+    order: 42
+  },
+  {
+    coords: [ 25.595124, 91.8842347 ],
+    pid: 'high-five',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/high-five.jpg',
+    title: 'High Five',
+    order: 41
+  },
+  {
+    coords: [ 34.062042, -118.301165 ],
+    pid: 'los-angeles',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/los-angeles.jpg',
+    title: 'Los Angeles',
+    order: 40
+  },
+  {
+    coords: [ -23.5454177, -46.6435733 ],
+    pid: 'sao-paulo',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/sao-paulo.jpg',
+    title: 'São Paulo',
+    order: 39
+  },
+  {
+    coords: [ -23.5563054, -46.6861153 ],
+    pid: 'alley-graffiti',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/alley-graffiti.jpg',
+    title: 'Alley Graffiti',
+    order: 38
+  },
+  {
+    coords: [ -23.550732, -46.677741 ],
+    pid: 'sumare',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/sumare.jpg',
+    title: 'Sumaré',
+    order: 37
+  },
+  {
+    coords: [ -23.54684, -46.637773 ],
+    pid: 'viaduto-do-cha',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/viaduto-do-cha.jpg',
+    title: 'Viaduto do Chá',
+    order: 36
+  },
+  {
+    coords: [ 37.802976, -122.275021 ],
+    pid: 'oakland-parade',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/oakland-parade.jpg',
+    title: 'Parade',
+    order: 35
+  },
+  {
+    coords: [ 9.964732, 76.239949 ],
+    pid: 'cochin',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/cochin.jpg',
+    title: 'Cochin',
+    order: 34
+  },
+  {
+    coords: [ 11.818059, 75.434905 ],
+    pid: 'road-work',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/road-work.jpg',
+    title: 'Road Work',
+    order: 33
+  },
+  {
+    coords: [ 15.475395, 73.80656 ],
+    pid: 'panaji-fishermen',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/panaji-fishermen.jpg',
+    title: 'Panaji Fishermen',
+    order: 32
+  },
+  {
+    coords: [ 15.086792, 74.030879 ],
+    pid: 'mine',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/mine.jpg',
+    title: 'Mine',
+    order: 31
+  },
+  {
+    coords: [ 26.443193, 71.211737 ],
+    pid: 'rajasthan-children',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/rajasthan-children.jpg',
+    title: 'Rajasthan Children',
+    order: 30
+  },
+  {
+    coords: [ 26.917995, 70.907156 ],
+    pid: 'mechanic',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/mechanic.jpg',
+    title: 'Mechanic',
+    order: 29
+  },
+  {
+    coords: [ 26.828032, 72.244785 ],
+    pid: 'rajasthan',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/rajasthan.jpg',
+    title: 'Rajasthan',
+    order: 28
+  },
+  {
+    pid: 'alpine-country',
+    title: 'Alpine Country',
+    vid: 'kSgdcwNGMDE',
+    order: 27
+  },
+  {
+    coords: [ 48.136278, 11.574859 ],
+    pid: 'alter-peter',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/alter-peter.jpg',
+    title: 'Alter Peter',
+    order: 26
+  },
+  {
+    coords: [ 48.136278, 11.574859 ],
+    pid: 'munich-candles',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/munich-candles.jpg',
+    title: 'Munich Candles',
+    order: 25
+  },
+  {
+    coords: [ 48.136278, 11.5748593 ],
+    pid: 'petersplatz',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/petersplatz.jpg',
+    title: 'Petersplatz',
+    order: 24
+  },
+  {
+    coords: [ 47.269089, 11.390601 ],
+    pid: 'innsbruck',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/innsbruck.jpg',
+    title: 'Innsbruck',
+    order: 23
+  },
+  {
+    coords: [ 47.263097, 11.296177 ],
+    pid: 'tyrol',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/tyrol.jpg',
+    title: 'Tyrol',
+    order: 22
+  },
+  {
+    coords: [ 38.876361, -77.07287 ],
+    pid: 'memorial-amphitheater',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/memorial-amphitheater.jpg',
+    title: 'Memorial Amphitheater',
+    order: 21
+  },
+  { pid: 'japan', title: 'Japan', vid: 'Y2vBeoIGkJ8', order: 20 },
+  {
+    coords: [ 34.701665, 135.190401 ],
+    pid: 'kobe',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/kobe.jpg',
+    title: 'Kobe',
+    order: 19
+  },
+  {
+    coords: [ 34.96714, 135.775158 ],
+    pid: 'shrine-of-inari',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/shrine-of-inari.jpg',
+    title: 'Shrine of Inari',
+    order: 18
+  },
+  {
+    coords: [ 35.009183, 135.674406 ],
+    pid: 'kyoto-monkey-one',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/kyoto-monkey-one.jpg',
+    title: 'Kyoto Monkey (1)',
+    order: 17
+  },
+  {
+    coords: [ 35.009183, 135.674406 ],
+    pid: 'kyoto-monkey-two',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/kyoto-monkey-two.jpg',
+    title: 'Kyoto Monkey (2)',
+    order: 16
+  },
+  {
+    coords: [ 34.221779, 135.605965 ],
+    pid: 'koyasan-monk',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/koyasan-monk.jpg',
+    title: 'Koyasan Monk',
+    order: 15
+  },
+  {
+    coords: [ 34.686127, 135.840618 ],
+    pid: 'together-in-japan',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/together-in-japan.jpg',
+    title: 'Together in Japan',
+    order: 14
+  },
+  {
+    coords: [ 34.678882, 135.820849 ],
+    pid: 'nara-guesthouse',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/nara-guesthouse.jpg',
+    title: 'Nara Guesthouse',
+    order: 13
+  },
+  {
+    coords: [ 34.701718, 135.189699 ],
+    pid: 'kobe-rooftops',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/kobe-rooftops.jpg',
+    title: 'Kobe Rooftops',
+    order: 12
+  },
+  {
+    coords: [ 39.989262, 116.276915 ],
+    pid: 'bicyclist',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/bicyclist.jpg',
+    title: 'Bicyclist',
+    order: 11
+  },
+  {
+    coords: [ 37.85566, -122.475801 ],
+    pid: 'sausalito',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/sausalito.jpg',
+    title: 'Sausalito',
+    order: 10
+  },
+  {
+    pid: 'spain-france',
+    title: 'Spain & France',
+    vid: 'E0PDfwfY2UE',
+    order: 9
+  },
+  {
+    coords: [ 43.483093, -1.566253 ],
+    pid: 'biarritz',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/biarritz.jpg',
+    title: 'Biarritz',
+    order: 8
+  },
+  {
+    coords: [ 43.482703, -1.563961 ],
+    pid: 'gazebo',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/gazebo.jpg',
+    title: 'Gazebo',
+    order: 7
+  },
+  {
+    coords: [ 35.13852, -120.643895 ],
+    pid: 'pismo',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/pismo.jpg',
+    title: 'Pismo',
+    order: 6
+  },
+  {
+    coords: [ 18.935349, 72.834023 ],
+    pid: 'mumbai',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/mumbai.jpg',
+    title: 'Mumbai',
+    order: 5
+  },
+  {
+    coords: [ 28.633557, 77.220604 ],
+    pid: 'delhi',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/delhi.jpg',
+    title: 'Delhi',
+    order: 4
+  },
+  {
+    coords: [ 53.349805, -6.26031 ],
+    pid: 'dublin-flower',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/dublin-flower.jpg',
+    title: 'Dublin Flower',
+    order: 3
+  },
+  {
+    coords: [ 38.876284, -77.070894 ],
+    pid: 'arlington-cross',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/arlington-cross.jpg',
+    title: 'Arlington Cross',
+    order: 2
+  },
+  {
+    coords: [ 40.146864, -83.036299 ],
+    pid: 'spring',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/spring.jpg',
+    title: 'Spring',
+    order: 1
+  },
+  {
+    coords: { '0': 37.7749, '1': 122.4194 },
+    pid: 'about-sept-2020',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/about-sept-2020.jpg',
+    title: 'About Me',
+    order: 0,
+    vid: ''
+  },
+  {
+    coords: [ 37.801636, -122.27636 ],
+    pid: 'about',
+    source: 'https://s3-us-west-2.amazonaws.com/alexhancock-photos/about.jpg',
+    title: 'About',
+    order: 0
+  }
+];
